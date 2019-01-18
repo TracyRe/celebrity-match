95b44332296a3467798dd682e2de8ff6a1d4ae5b
Gary Bermudez and Tracy Jo Reith <tracy@>
2019-01-17 16:16:52 -0800

    Style button, add javascript to clear form

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $("#love").submit (function(event) {
+    var food = parseInt($("#food").val());
+    var fun = parseInt($("#fun").val());
+    var color = parseInt($("#color").val());
+    var result = food + fun + color ;
+
+  if (result < 5) {
+    $('#match1').show();
+    $('#match2').hide();
+    $('#match3').hide();
+  } else if (result > 4 && result < 8) {
+    $('#match2').show();
+    $('#match1').hide();
+    $('#match3').hide();
+  } else {
+    $('#match3').show();
+    $('#match1').hide();
+    $('#match2').hide();
+  }
+  event.preventDefault();
+
+
+    $("#food").val("");
+    $("#fun").val("");
+    $("#color").val("");
+});
+
+
+
+
+});
